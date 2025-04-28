7ca9a4e30759ee204f63901925c59afe7ff551cd
hadoizz <82988056+hadoizz@users.noreply.github.com>
2025-04-28 21:29:06 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
-  publicRoutes: ["/", "/plans/:planId/community-plan(.*)", "/community-plans"],
-});
+// Use Clerk's authMiddleware without the publicRoutes configuration
+export default authMiddleware();
 
+// Configure the matcher to allow Google to crawl your pages and exclude static files
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"], // Allow Google to crawl homepage and necessary routes
 };
